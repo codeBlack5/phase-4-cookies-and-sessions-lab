73d263138dbd5fa8a6aaea16716c21507d54720c
Steve <stevekoech8@gmail.com>
2023-03-24 00:06:58 +0300

    Completed Assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,17 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    session[:page_views] ||= 0
+    if session[:page_views] < 3
+      session[:page_views] +=1
+      render json: Article.find(params[:id])
+    else
+      session[:page_views] += 1
+      render json: {error: "Maximum pageview limit reached"}, status: :unauthorized
+  
+    end
   end
-
+  
   private
 
   def record_not_found
